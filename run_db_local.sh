301843f3eec29fca1c4068eefc6ec0b866dafd80
Vlad Chira <46445620+VladChira@users.noreply.github.com>
2025-05-04 00:51:52 +0300

    Fixed run_db_local.sh not using env secrets

--- run_db_local.sh
+++ run_db_local.sh
@@ -1,9 +1,7 @@
 sudo docker run -d \
   --name chatapp-db \
   --restart unless-stopped \
-  -e POSTGRES_USER="${POSTGRES_USER}" \
-  -e POSTGRES_PASSWORD="${POSTGRES_PASSWORD}" \
-  -e POSTGRES_DB="${POSTGRES_DB}" \
   -v pgdata:/var/lib/postgresql/data \
   -p 5432:5432 \
+  --env-file .env \
   postgres:15
